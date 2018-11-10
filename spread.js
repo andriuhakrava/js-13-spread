43a8293492368cb49491a7cd9b276fc941359e11
andriuhakrava <andriuhakrava@gmail.com>
2018-11-10 09:41:14 +0200

    first upload

---
+++ spread.js
@@ -0,0 +1,77 @@
+// 1. Добавте до гарбуза властивість "ціна за кілограм" та метод, який би обчислював вартість гарбуза.
+
+	let pumpkin = {
+	  color: 'green',
+	  weight: '34 kg',
+	  'ціна за кілограм': '26 грн',
+	  pricePumpkin: function(){
+	  	return parseFloat(this.weight) * parseFloat(this['ціна за кілограм']) + ' грн.';
+	  }
+	};
+	console.groupCollapsed('Вартість гарбуза:');
+	console.log(pumpkin.pricePumpkin());
+	console.groupEnd();
+
+/* 2. Створіть метод, який би повертав копію гарбуза. Можна застосувати не глибоке копіювання.
+	Створіть масив з п'яти гарбузів, використовуючи даний метод копіювання. */
+
+	let arrCopiedPumpkins = [];
+	for (let i = 0; i < 5; i++){
+		arrCopiedPumpkins.push(copyPumpkin());
+	}
+
+	function copyPumpkin(){
+		let obj = {};
+		for (let key in pumpkin) {
+		  obj[key] = pumpkin[key];
+		}
+		return obj;
+	}
+
+	console.groupCollapsed('Масив з 5 скопійованих гарбузів:');
+	console.log(arrCopiedPumpkins);
+	console.groupEnd();
+
+/* 3. Задайте всім гарбузам випадкову вагу від 5 до 20 кг.
+	Виведіть в консоль номер гарбуза та його ціну. */
+
+	function randomWeight(){
+		return Math.floor(Math.random()*16) + 5;
+	}
+
+	for (let i = 0; i < 5; i++){
+		arrCopiedPumpkins[i].weight = `${randomWeight()} кг`;
+		let price = arrCopiedPumpkins[i].pricePumpkin();
+		console.log(`Ціна ${i + 1} гарбуза - ${price}`);
+	}
+
+/* 4. Виведіть в консоль вагу всіх гарбузів:
+	12 кг; 19 кг; 11 кг; 16 кг; 18 кг
+	Спробуйте обійтися однією командою: всі дії вписати в дужки:
+	console.log(...); */
+
+	console.groupCollapsed('Вага всіх гарбузів:');
+	console.log(arrCopiedPumpkins.map((pum) => pum.weight).join('; '));
+	console.groupEnd();
+
+/* 5. Створіть функцію, що отримує у якості аргументів марки автомобілів 
+	і виводить їх в консоль в одну строку через кому та пробіл. */
+
+	function carBrand(...brands){
+		console.groupCollapsed('Марки автомобілів в одну строку через кому та пробіл:');
+		console.log(brands.join(', '));
+		console.groupEnd();
+	}
+
+	carBrand('BMW', 'Opel', 'Chevrolet', 'Skoda');
+
+/* 6. Створіть функцію, що отримує у якості аргументів гарбузи 
+	і виводить в консоль в стовбчик їх ціну. Передайте в функцію масив гарбузів. */
+
+	function showPumpkinsPrice(arr){
+		arr.forEach(function(p){
+			console.log(p.pricePumpkin());
+		});
+	}
+
+	showPumpkinsPrice(arrCopiedPumpkins);
